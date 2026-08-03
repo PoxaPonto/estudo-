25a57ce5427b4dd42155b618e3520c21a50e8ce1
Poxa <poxaponto@gmail.com>
2026-08-02 22:28:56 -0300

    a

--- serve.js
+++ serve.js
@@ -8,12 +8,29 @@
 // server.listen(3000)
 
 import { fastify } from "fastify";
-
+import { DatabaseMemory } from "./database-memory.js";
 const serve = fastfy()
 
-server.post("/viudeos", () => {
-  return `Hello Word`
+server.post("/viudeos", (request, reply) => {
+    
+
+
+
+  database.create({
+  title: "Jackson",
+  description: "ele gosta",
+  duration: 1000,
+ 
+  })
+  
+  console.log(datababse.list())
+  
+   return reply.status(201).send()
+
 })
+
+
+
 
 server.get("/videos", () => {
   return `Hello Poxa`
